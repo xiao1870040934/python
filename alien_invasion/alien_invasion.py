d3a605afe46b9d7bc780a5a8f8c5d802ce49f245
xiaoge520 <1870040934@qq.com>
2022-04-28 11:24:42 +0800

    添加飞船头像

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 
 import pygame as pygame
 from settings import Settings
+from ship import Ship
 
 
 class ALienInvasion:
@@ -16,6 +17,8 @@ class ALienInvasion:
             (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+
         # 设置背景色
         self.bg_color = (230, 230, 230)
 
@@ -29,6 +32,7 @@ class ALienInvasion:
 
             # 每次循环都重新绘制屏幕
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # 让最近绘制的屏幕可见
             pygame.display.flip()
