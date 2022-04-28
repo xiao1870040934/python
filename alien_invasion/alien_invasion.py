2be7573b421e006a49a65c6cf2bef63151af6a45
xiaoge520 <1870040934@qq.com>
2022-04-28 11:30:44 +0800

    重构方法

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -25,17 +25,21 @@ class ALienInvasion:
     def run_game(self):
         """"开始游戏的主循环"""
         while True:
-            # 监视键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self._update_screen()
 
-            # 每次循环都重新绘制屏幕
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
+    def _check_events(self):
+        """"响应按键和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
 
-            # 让最近绘制的屏幕可见
-            pygame.display.flip()
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 
 if __name__ == '__main__':
