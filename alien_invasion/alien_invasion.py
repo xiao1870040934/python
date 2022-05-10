eedfbe1f62f324e934dab9bc9ae72ad4e04473ca
xiaoge520 <1870040934@qq.com>
2022-05-10 08:21:43 +0800

    确定运行游戏的哪些部分

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -36,9 +36,10 @@ class ALienInvasion:
         """"开始游戏的主循环"""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+            if self.stats.game_active:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
             self._update_screen()
 
     def _check_events(self):
